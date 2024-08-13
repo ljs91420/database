b0944995f59e074d969416ae8697ef0fd947053e
이재상 <>
2024-08-13 17:43:42 +0900

    83일차

---
+++ 연습용계정6.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ProfileImage (
+    id NUMBER(8) CONSTRAINT profile_image_id_pk PRIMARY KEY,
+    originalFilename VARCHAR2(100),
+    storedFilename VARCHAR2(100)
+);
+
+CREATE SEQUENCE profile_image_seq NOCACHE NOCYCLE;
+
+DESC profileImage;
